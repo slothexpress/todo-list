9bc436a8f9cb14fd3b5b072d988f59271ec3ff2a
slothexpress <hellosami@protonmail.com>
2023-03-08 14:49:01 +0100

    Add useEffect hook for local storage/save session

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import './App.css';
 import TodoList from './TodoList';
 import { v4 as uuidv4 } from 'uuid';
+
+const LOCAL_STORAGE_KEY = 'todoApp.todos';
 
 function App() {
 
   const [todos, setTodos] = useState([]);
   const todoNameRef = useRef();
+
+  /* called once, on load */
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+   
+    if(storedTodos) {
+      setTodos(storedTodos);
+    }
+    
+  }, []);
+
+  /* called every time something in the todos array changes */
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos]);
 
   function handleAddTodo(event) {
     const name = todoNameRef.current.value;
